dff4c94d593be4783d1411844164b5c4540294fa
Peishan Lin <171846z@mymail.nyp.edu.sg>
2018-11-15 01:34:32 +0800

    testing

---
+++ app/src/main/java/sg/edu/nyp/mymail/a171846z/moviereview/RateMovie.kt
@@ -0,0 +1,16 @@
+package sg.edu.nyp.mymail.a171846z.moviereview
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.v7.widget.DialogTitle
+import org.intellij.lang.annotations.Language
+import java.util.*
+
+class RateMovie : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_rate_movie)
+    }
+
+}
